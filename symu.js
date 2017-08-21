58c5d7e94e583f55d75dea1eba79d41151d31ea9
JoaoVKS <joaokussler@gmail.com>
2017-08-21 20:42:38 -0300

    v1.2
    forget the AI, lets make a game:
    visual update;
    arrow movement added;
    added energy display.

--- symu.js
+++ symu.js
@@ -10,42 +10,91 @@ $.fn.redraw = function(){
 });
 };
 
-//KEYPRESS
-$(document).keypress(function(event){
-  //note : pra otimizar podemos passar o elemento 'you' para uma var
-  var time = 0;
-  var i = 0;
-  setInterval(function(){
-    if(i < 19){
-      move_to(0, i);
-      i++
-    }
-},1800);
+  
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37:
+        move_left();
+        console.log('left'); // left
+        break;
 
+        case 38:
+        move_up();
+        console.log('up'); // up
+        break;
+
+        case 39:
+        move_right();
+        console.log('right'); // right
+        break;
+
+        case 40:
+        move_down();
+        console.log('down'); // down
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
 });
+
 //FUNCTIONS
+
+function move_left()
+{
+   var tempx = $("#x").val();
+   var tempy = $("#y").val();
+   tempy = parseInt(tempy)-parseInt(1);
+   move_to(tempx, tempy);   
+}
+
+function move_right()
+{
+   var tempx = $("#x").val();
+   var tempy = $("#y").val();
+   tempy = parseInt(tempy)+parseInt(1);
+   move_to(tempx, tempy);   
+}
+
+function move_down()
+{
+   var tempx = $("#x").val();
+   var tempy = $("#y").val();
+   tempx = parseInt(tempx)+parseInt(1);
+   move_to(tempx, tempy);   
+}
+function move_up()
+{
+   var tempx = $("#x").val();
+   var tempy = $("#y").val();
+   tempx = parseInt(tempx)-parseInt(1);
+   move_to(tempx, tempy);   
+}
+
 function init()
 {
   $("#x").val(0);
   $("#y").val(0);
   constroi(20, 20);
   console.log("Started!");
-  move_to(0, 0);
   set_energy(100);
+  move_to(0, 0);
+  
 }
 
 function constroi(limit_y, limit_x)
 {
   var i = 0
   var x = 0;
-  var tt = "<table border='1' id='screen' style='width:100%;height:100%'>";
+  var tt = "<table border='1' class='field' id='screen' style=''>";
   for(i = 0; i<limit_y; i++)
   {
     tt += "<tr>";
     for(x = 0; x<limit_x; x++)
     {
-      tt += "<td x='"+i+"' y='"+x+"'></td>"
+      tt += "<td x='"+i+"' y='"+x+"'></td>";
     }
+    
     tt += "</tr>";
   }
   tt += "</table>";
@@ -69,21 +118,28 @@ function sleep(milliseconds) {
 
 function move_to(x, y)
 {
-  var tempx = $("#x").val();
-  var tempy = $("#y").val();
-  //$("td[x="+tempx+"][y="+tempy+"]").attr('style', 'background-color:#484848');
-  $("td[x="+tempx+"][y="+tempy+"]").attr("class", "rastro");
-  $("td[x="+tempx+"][y="+tempy+"]").text('');
-  $("#x").val(x);
-  $("#y").val(y);
-  //$("td[x="+x+"][y="+y+"]").attr('style', 'background-color:blue');
-  $("td[x="+x+"][y="+y+"]").attr("class", "aqui");
-  $("td[x="+x+"][y="+y+"]").text('');
-  console.log("location: x-" + x + " y-" + y);
-  var e = parseInt($("#energy").val()) - 1;
-  set_energy(e);
-  if(e <= 0)
+  var classe = $("td[x="+x+"][y="+y+"]").attr("class");
+
+  if($("td[x="+x+"][y="+y+"]").length > 0 && classe != "wall")
   {
-    console.log("Faleceu!");
+    var tempx = $("#x").val();
+    var tempy = $("#y").val();
+    //$("td[x="+tempx+"][y="+tempy+"]").attr('style', 'background-color:#484848');
+    $("td[x="+tempx+"][y="+tempy+"]").attr("class", "rastro");
+    $("td[x="+tempx+"][y="+tempy+"]").text('');
+    $("#x").val(x);
+    $("#y").val(y);
+    //$("td[x="+x+"][y="+y+"]").attr('style', 'background-color:blue');
+
+  
+    $("td[x="+x+"][y="+y+"]").attr("class", "aqui");
+    $("td[x="+x+"][y="+y+"]").text('');
+    console.log("location: x-" + x + " y-" + y);
+    var e = parseInt($("#energy").val()) - 1;
+    set_energy(e);
+    if(e <= 0)
+    {
+      console.log("Faleceu!");
+    }
   }
 }
